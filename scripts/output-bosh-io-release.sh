073e7b804bb8228047d7563c19d7c270c773f51c
Mathieu Massé <mathieu.masse@gmail.com>
2018-09-07 14:08:01 -0400

    Use correct directory for output

--- scripts/output-bosh-io-release.sh
+++ scripts/output-bosh-io-release.sh
@@ -4,5 +4,5 @@ set -e
 
 RELEASE_NAME=$1-$(cat incoming-release/version).tgz
 
-mkdir -p /data/repo/$2
-cp incoming-release/release.tgz /data/repo/$2/${RELEASE_NAME}
+mkdir -p /repo/$2
+cp incoming-release/release.tgz /repo/$2/${RELEASE_NAME}
