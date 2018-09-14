7fd292f5370d1caf99fc935048b99f6d84d802c4
Mathieu Massé <mathieu.masse@gmail.com>
2018-09-14 14:04:19 -0400

    Fix cf-cli filename

--- scripts/fetch-cf-cli-release.sh
+++ scripts/fetch-cf-cli-release.sh
@@ -6,7 +6,5 @@ SOURCE_FOLDER=$1
 OUTPUT_FOLDER=$2
 VERSION=$(cat ${SOURCE_FOLDER}/version)
 
-pushd  ${OUTPUT_FOLDER}
-curl -vvv -LO "https://packages.cloudfoundry.org/stable?release=redhat64&version=${VERSION}&source=github-rel"
-popd
+curl -Lo ${OUTPUT_FOLDER}/cf-cli-installer_${VERSION}_x86-64.rpm "https://packages.cloudfoundry.org/stable?release=redhat64&version=${VERSION}&source=github-rel"
 cp ${SOURCE_FOLDER}/body ${OUTPUT_FOLDER}/cf-cli-${VERSION}-release.md
