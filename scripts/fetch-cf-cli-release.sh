e3e73800eafdbbfaa858f557bbd3e74f94c07d6c
Mathieu Massé <mathieu.masse@gmail.com>
2018-09-14 13:27:58 -0400

    Fixes for cf-cli

--- scripts/fetch-cf-cli-release.sh
+++ scripts/fetch-cf-cli-release.sh
@@ -6,6 +6,7 @@ SOURCE_FOLDER=$1
 OUTPUT_FOLDER=$2
 VERSION=$(cat ${SOURCE_FOLDER}/version)
 
-cd ${OUTPUT_FOLDER}
+pushd  ${OUTPUT_FOLDER}
 curl -LO https://packages.cloudfoundry.org/stable?release=redhat64&version=${VERSION}&source=github-rel
+popd
 cp ${SOURCE_FOLDER}/body ${OUTPUT_FOLDER}/cf-cli-${VERSION}-release.md
