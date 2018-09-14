9eb6006166e77dd8e9a5406d9904d4b2306317a0
Mathieu Massé <mathieu.masse@gmail.com>
2018-09-14 13:46:34 -0400

    Quote cf-cli url

--- scripts/fetch-cf-cli-release.sh
+++ scripts/fetch-cf-cli-release.sh
@@ -7,6 +7,6 @@ OUTPUT_FOLDER=$2
 VERSION=$(cat ${SOURCE_FOLDER}/version)
 
 pushd  ${OUTPUT_FOLDER}
-curl -LO https://packages.cloudfoundry.org/stable?release=redhat64&version=${VERSION}&source=github-rel
+curl -LO "https://packages.cloudfoundry.org/stable?release=redhat64&version=${VERSION}&source=github-rel"
 popd
 cp ${SOURCE_FOLDER}/body ${OUTPUT_FOLDER}/cf-cli-${VERSION}-release.md
