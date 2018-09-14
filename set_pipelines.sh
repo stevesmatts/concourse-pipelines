9f931296b14277401c4e64225b33f6740fa8934d
Mathieu Massé <mathieu.masse@gmail.com>
2018-09-14 15:22:01 -0400

    Updated set_pipeline.sh script to discover pipelines automaticly

--- set_pipelines.sh
+++ set_pipelines.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-fly -t mirror sp -n -p mattermost -c pipelines/mattermost.yml --load-vars-from credentials.yml
-fly -t mirror up -p mattermost
+CREDENTIAL_FILE=credentials.yml
+TARGET=mirror
 
-fly -t mirror sp -n -p rabbitmq -c pipelines/rabbitmq.yml --load-vars-from credentials.yml
-fly -t mirror up -p rabbitmq
+PIPELINE_FILES=pipelines/*
 
-fly -t mirror sp -n -p tools -c pipelines/tools.yml --load-vars-from credentials.yml
-fly -t mirror up -p tools
-
-fly -t mirror sp -n -p memcache -c pipelines/memcache.yml --load-vars-from credentials.yml
-fly -t mirror up -p memcache
-
-fly -t mirror sp -n -p mysql -c pipelines/mysql.yml --load-vars-from credentials.yml
-fly -t mirror up -p mysql
+for FILE in ${PIPELINE_FILES}
+do
+    PIPELINE_FILE=$(basename -- "$FILE")
+    PIPELINE="${PIPELINE_FILE%.*}"
+    fly -t ${TARGET} sp -n -p ${PIPELINE} -c pipelines/${PIPELINE_FILE} --load-vars-from credentials.yml
+    fly -t mirror up -p ${PIPELINE}
+done
