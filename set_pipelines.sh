d37b3b5ae99c62939ee5820db4e81d8990320076
Mathieu Massé <mathieu.masse@gmail.com>
2018-09-14 10:31:50 -0400

    Add rabbitmq pipeline and set non-interactive

--- set_pipelines.sh
+++ set_pipelines.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-fly -t mirror sp -p mattermost -c pipelines/mattermost.yml --load-vars-from credentials.yml
+fly -t mirror sp -n -p mattermost -c pipelines/mattermost.yml --load-vars-from credentials.yml
 fly -t mirror up -p mattermost
+
+fly -t mirror sp -n -p rabbitmq -c pipelines/rabbitmq.yml --load-vars-from credentials.yml
+fly -t mirror up -p rabbitmq
