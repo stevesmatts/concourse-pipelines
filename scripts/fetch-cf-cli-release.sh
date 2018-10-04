407b015d238bdad5979fde3489f6d1228d3a5516
Mathieu Massé <mathieu.masse@gmail.com>
2018-10-04 10:51:49 -0400

    Fetch the CF-CLI windows version

--- scripts/fetch-cf-cli-release.sh
+++ scripts/fetch-cf-cli-release.sh
@@ -7,4 +7,5 @@ OUTPUT_FOLDER=$2
 VERSION=$(cat ${SOURCE_FOLDER}/version)
 
 curl -Lo ${OUTPUT_FOLDER}/cf-cli-installer_${VERSION}_x86-64.rpm "https://packages.cloudfoundry.org/stable?release=redhat64&version=${VERSION}&source=github-rel"
+curl -Lo ${OUTPUT_FOLDER}/cf-cli-installer_${VERSION}_winx64.zip "https://packages.cloudfoundry.org/stable?release=windows64&version=${VERSION}&source=github-rel"
 cp ${SOURCE_FOLDER}/body ${OUTPUT_FOLDER}/cf-cli-${VERSION}-release.md
