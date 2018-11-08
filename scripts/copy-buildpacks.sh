768a5c81cfb83eea06bfde31b1181168059b0b0d
Mathieu Massé <mathieu.masse@gmail.com>
2018-11-08 14:20:32 -0500

    Remove the rsync tasks

--- scripts/copy-buildpacks.sh
+++ scripts/copy-buildpacks.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 
 set -e
+set -o pipefail
 
-SOURCE_FOLDER=$1
-OUTPUT_FOLDER=$2
+SERVER=$1
+USERNAME=$2
+KEY_BASE64=$3
+OUTPUT_DIRECTORY=$4
 
-cp ${SOURCE_FOLDER}/*.zip ${OUTPUT_FOLDER}/.
+mkdir ./temp
+find . -type f -name '*.zip' -exec mv -t ./temp/ {} \+
+
+mkdir -p ~/.ssh
+echo -e ${KEY_BASE64} | base64 --decode > ~/.ssh/server_key
+echo -e "Host ${SERVER}\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
+chmod -R 600 ~/.ssh
+
+CMD="ssh -i ~/.ssh/server_key ${USERNAME}@${SERVER} mkdir -p ${OUTPUT_DIRECTORY}"
+echo ${CMD} 1>&2
+eval ${CMD} 1>&2
+
+RSYNC_CMD="rsync -av -e 'ssh -i ~/.ssh/server_key' ./temp/ ${USERNAME}@${SERVER}:${OUTPUT_DIRECTORY}"
+echo ${RSYNC_CMD} 1>&2
+eval ${RSYNC_CMD} 1>&2
