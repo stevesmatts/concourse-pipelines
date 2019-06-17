d62dcc159cdf3ffa218e8d6f447e21dcfc9f3c90
Mathieu Massé <mathieu.masse@gmail.com>
2019-06-17 08:52:19 -0400

    Update RabbitMQ pipeline with latest requirements

--- scripts/process-rabbitmq.sh
+++ scripts/process-rabbitmq.sh
@@ -6,9 +6,15 @@ set -o pipefail
 BOSH_IO_SCRIPT=$1
 RABBIT_SOURCE=$2
 RABBIT_BROKER_SOURCE=$3
-CF_CLI_SOURCE=$4
+RABBIT_SMOKE_TESTS=$4
+CF_CLI_SOURCE=$5
+ROUTING=$6
+BPM=$7
 OUTPUT_DIRECTORY=$5
 
 source ${BOSH_IO_SCRIPT} ${RABBIT_SOURCE} ${OUTPUT_DIRECTORY} cf-rabbitmq-release
 source ${BOSH_IO_SCRIPT} ${RABBIT_BROKER_SOURCE} ${OUTPUT_DIRECTORY} cf-rabbitmq-multitenant-broker-release
+source ${BOSH_IO_SCRIPT} ${RABBIT_SMOKE_TESTS} ${OUTPUT_DIRECTORY} cf-rabbitmq-smoke-tests-release
 source ${BOSH_IO_SCRIPT} ${CF_CLI_SOURCE} ${OUTPUT_DIRECTORY} cf-cli-release
+source ${BOSH_IO_SCRIPT} ${ROUTING} ${OUTPUT_DIRECTORY} cf-routing-release
+source ${BOSH_IO_SCRIPT} ${BPM} ${OUTPUT_DIRECTORY} bpm-release
