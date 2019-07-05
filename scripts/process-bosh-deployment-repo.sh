c229ae0703c94fe9a379c546bf7c1fe4e248787a
Mathieu Massé <mathieu.masse@gmail.com>
2019-07-05 14:10:25 -0400

    Fetch Bosh-Lite resources

--- scripts/process-bosh-deployment-repo.sh
+++ scripts/process-bosh-deployment-repo.sh
@@ -9,7 +9,8 @@ STEMCELL_FOLDER=$4
 BOSH_IO_SCRIPT=$5
 DNS_SOURCE=$6
 
-echo Generate a manifest with all the required ops files
+# VSPHERE DIRECTOR
+echo Generate a manifest with all the required ops files for a vSphere director
 bosh int ${REPO_LOCATION}/bosh.yml \
   -o ${REPO_LOCATION}/vsphere/cpi.yml \
   -o ${REPO_LOCATION}/misc/dns.yml \
@@ -29,4 +30,21 @@ echo Fetching the stemcell ${STEMCELL_URL}
 curl --silent -LOJ --retry 5 ${STEMCELL_URL}
 popd > /dev/null
 
+# BOSH-LITE DIRECTOR
+echo Generate a manifest with all the required ops files for a Bosh-Lite director
+bosh int ${REPO_LOCATION}/bosh.yml \
+  -o ${REPO_LOCATION}/virtualbox/cpi.yml \
+  -o ${REPO_LOCATION}/bosh-lite.yml >> manifest-lite.yml
+
+echo Fetching releases
+source ${FETCH_SCRIPT} manifest-lite.yml ${RELEASE_FOLDER}
+
+STEMCELL_URL=$(bosh int manifest-lite.yml --path /resource_pools/name=vms/stemcell/url)
+
+pushd ${STEMCELL_FOLDER} > /dev/null
+echo Fetching the stemcell ${STEMCELL_URL}
+curl --silent -LOJ --retry 5 ${STEMCELL_URL}
+popd > /dev/null
+
+# BOSH DNS RELEASE
 source ${BOSH_IO_SCRIPT} ${DNS_SOURCE} ${RELEASE_FOLDER} bosh-dns-release
