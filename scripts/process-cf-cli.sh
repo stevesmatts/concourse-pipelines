983a4311d0d70f7ff90b5fe05e16474674af2ed4
Mathieu Massé <mathieu.masse@gmail.com>
2020-02-11 09:46:00 -0500

    Download all CLIs to subfolder with version

--- scripts/process-cf-cli.sh
+++ scripts/process-cf-cli.sh
@@ -7,6 +7,9 @@ CF_CLI_SOURCE=$1
 CF_CLI_OUTPUT=$2
 CF_CLI_VERSION=$(cat ${CF_CLI_SOURCE}/version)
 
+CF_CLI_OUTPUT=${CF_CLI_OUTPUT}/${CF_CLI_VERSION}
+mkdir -p ${CF_CLI_OUTPUT}
+
 # Process CF CLI
 echo "Processing CF CLI ${CF_CLI_VERSION} Linux client (RPM installer)"
 curl --silent --retry 5 -Lo ${CF_CLI_OUTPUT}/cf-cli-installer_${CF_CLI_VERSION}_x86-64.rpm "https://packages.cloudfoundry.org/stable?release=redhat64&version=${CF_CLI_VERSION}&source=github-rel"
@@ -16,5 +19,5 @@ echo "Processing CF CLI ${CF_CLI_VERSION} Windows client (standalone)"
 curl --silent --retry 5 -Lo ${CF_CLI_OUTPUT}/cf-cli_${CF_CLI_VERSION}_winx64.zip "https://packages.cloudfoundry.org/stable?release=windows64-exe&version=${CF_CLI_VERSION}&source=github-rel"
 if [[ -f ${CF_CLI_SOURCE}/body ]]; then
   echo "Processing CF CLI ${CF_CLI_VERSION} release notes"
-  cp ${CF_CLI_SOURCE}/body ${CF_CLI_OUTPUT}/cf-cli-installer_${CF_CLI_VERSION}-release.md
+  cp ${CF_CLI_SOURCE}/body ${CF_CLI_OUTPUT}/release.md
 fi
