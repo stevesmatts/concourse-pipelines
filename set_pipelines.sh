90978a1ddbabd4b488147a459eed169a9d231d77
Mathieu Massé <mymasse@users.noreply.github.com>
2020-05-12 06:07:33 -0400

    Cleanup the set pipeline script (#130)
    - Allow custom target name (defaults to mirror if not set).
    - Allow a different name for pipeline
    - Delete the set_pipeline2.sh which should never have been committed!

--- set_pipelines.sh
+++ set_pipelines.sh
@@ -1,14 +1,41 @@
 #!/usr/bin/env bash
 
+set -e
+set -o pipefail
+
 CREDENTIAL_FILE=credentials.yml
-TARGET=mirror
 
-PIPELINE_FILES=pipelines/*
+if [[ "$#" -eq 1 ]]; then
+  TARGET=$1
+else
+  printf "Defaulting to target: mirror\n"
+  TARGET=mirror
+fi
 
-for FILE in ${PIPELINE_FILES}
-do
-    PIPELINE_FILE=$(basename -- "$FILE")
-    PIPELINE="${PIPELINE_FILE%.*}"
-    fly -t ${TARGET} sp -n -p ${PIPELINE} -c pipelines/${PIPELINE_FILE} --load-vars-from credentials.yml
-    fly -t mirror up -p ${PIPELINE}
-done
+if ! command -v fly > /dev/null 2>&1; then
+  echo "fly must be installed before running this script!"
+  exit 1
+fi
+
+configure_pipeline() {
+  NAME=$1
+  CONFIG_FILE=$2
+
+  printf "Setting pipeline: %s\n" ${NAME}
+  fly -t ${TARGET} set-pipeline -n -p ${NAME} -c ${CONFIG_FILE} --load-vars-from ${CREDENTIAL_FILE}
+  fly -t ${TARGET} unpause-pipeline -p ${NAME} > /dev/null
+}
+
+configure_pipeline buildpacks pipelines/buildpacks.yml
+configure_pipeline cf pipelines/cf.yml
+configure_pipeline common pipelines/common.yml
+configure_pipeline concourse pipelines/concourse.yml
+configure_pipeline director pipelines/director.yml
+configure_pipeline doomsday pipelines/doomsday.yml
+configure_pipeline logsearch pipelines/logsearch.yml
+configure_pipeline mattermost pipelines/mattermost.yml
+configure_pipeline minio pipelines/minio.yml
+configure_pipeline mysql pipelines/mysql.yml
+configure_pipeline rabbitmq pipelines/rabbitmq.yml
+configure_pipeline redis pipelines/redis.yml
+configure_pipeline test-deployment pipelines/test-deployment.yml
