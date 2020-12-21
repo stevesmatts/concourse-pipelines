589bba82942bf974387758e106edc0dceb69427d
Mathieu Masse <mathieu.masse@gmail.com>
2020-12-21 13:20:40 -0500

    Add Prometheus pipeline
    Fixes: VITE-821

--- set_pipelines.sh
+++ set_pipelines.sh
@@ -12,7 +12,7 @@ else
   TARGET=mirror
 fi
 
-if ! command -v fly > /dev/null 2>&1; then
+if ! command -v fly >/dev/null 2>&1; then
   echo "fly must be installed before running this script!"
   exit 1
 fi
@@ -23,7 +23,7 @@ configure_pipeline() {
 
   printf "Setting pipeline: %s\n" ${NAME}
   fly -t ${TARGET} set-pipeline -n -p ${NAME} -c ${CONFIG_FILE} --load-vars-from ${CREDENTIAL_FILE}
-  fly -t ${TARGET} unpause-pipeline -p ${NAME} > /dev/null
+  fly -t ${TARGET} unpause-pipeline -p ${NAME} >/dev/null
 }
 
 configure_pipeline buildpacks pipelines/buildpacks.yml
@@ -36,6 +36,7 @@ configure_pipeline logsearch pipelines/logsearch.yml
 configure_pipeline mattermost pipelines/mattermost.yml
 configure_pipeline minio pipelines/minio.yml
 configure_pipeline mysql pipelines/mysql.yml
+configure_pipeline prometheus pipelines/prometheus.yml
 configure_pipeline rabbitmq pipelines/rabbitmq.yml
 configure_pipeline redis pipelines/redis.yml
 configure_pipeline test-deployment pipelines/test-deployment.yml
